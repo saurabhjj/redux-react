138670d97326c68c0c0a6fc7e9f99844d8390944
saurabhjj <saurabhjj@gmail.com>
2017-09-13 23:51:56 +0530

    setting up redux

--- src/app/components/User.js
+++ src/app/components/User.js
@@ -17,8 +17,7 @@ export class User extends React.Component {
         return (
             <div>
                 <h3>The User Page</h3>
-                <p> User ID: {this.props.match.params.id}</p>
-                <button onClick={this.onNavigateHome} className="btn btn-primary">Go Home !</button>
+                <p> User ID: {this.props.username}</p>
             </div>
         );
     }
--- src/app/index.js
+++ src/app/index.js
@@ -1,23 +1,31 @@
 import React from "react";
 import {render} from "react-dom";
-import {BrowserRouter, Route,Switch} from 'react-router-dom';
 
-import {Root} from "./components/Root";
-
+import { User } from './components/User';
+import { Main } from './components/Main';
 
 class App extends React.Component {
-  
-    render(){
-      
-     return (       
-        <BrowserRouter >
-            <Switch>
-                <Route path="/" component={Root} />
-            </Switch>
-        </BrowserRouter>
-     )  ;
+    constructor() {
+        super();
+        this.state = {
+            username: "Max"
+        };
     }
 
+    changeUsername(newName) {
+        this.setState({
+            username: newName
+        });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Main changeUsername={this.changeUsername.bind(this)}/>
+                <User username={this.state.username}/>
+            </div>
+        );
+    }
 }
 
-render(<App />,window.document.getElementById("app"));
+render(<App />, window.document.getElementById('app'));
