4e10b4a48582c07735c25905f4a05d910b27762c
saurabhjj <saurabhjj@gmail.com>
2017-09-14 23:14:05 +0530

    learing create and combineReducers

--- src/app/index.js
+++ src/app/index.js
@@ -1,31 +1,108 @@
-import React from "react";
-import {render} from "react-dom";
+// import React from "react";
+// import {render} from "react-dom";
 
-import { User } from './components/User';
-import { Main } from './components/Main';
+// import { User } from './components/User';
+// import { Main } from './components/Main';
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            username: "Max"
+// class App extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             username: "Max"
+//         };
+//     }
+
+//     changeUsername(newName) {
+//         this.setState({
+//             username: newName
+//         });
+//     }
+
+//     render() {
+//         return (
+//             <div className="container">
+//                 <Main changeUsername={this.changeUsername.bind(this)}/>
+//                 <User username={this.state.username}/>
+//             </div>
+//         );
+//     }
+// }
+
+// render(<App />, window.document.getElementById('app'));
+
+import {createStore,combineReducers} from "redux" ;
+
+const mathReducer = (state={
+        result:1,
+        lastValues:[]
+    }, action) => {
+    switch (action.type){
+        case "ADD":
+            state={
+                ...state,
+                result:state.result + action.payload,
+                lastValues:[...state.lastValues,action.payload]
+            };
+            // state.lastValues.push(action.payload);
+            break;
+        case "SUBTRACT":
+        state={
+            ...state,
+            result:state.result - action.payload,
+            lastValues:[...state.lastValues,action.payload]
         };
+        // state.lastValues.push(action.payload);
+            break;
     }
+    return state;
+};
 
-    changeUsername(newName) {
-        this.setState({
-            username: newName
-        });
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <Main changeUsername={this.changeUsername.bind(this)}/>
-                <User username={this.state.username}/>
-            </div>
-        );
-    }
+const userReducer = (state={
+    name:"Max",
+    age:27
+}, action) => {
+switch (action.type){
+    case "SET_NAME":
+        state={
+            ...state,
+            name:action.payload
+        };
+        // state.lastValues.push(action.payload);
+        break;
+    case "SET_AGE":
+    state={
+        ...state,
+        age:action.payload
+    };
+    // state.lastValues.push(action.payload);
+        break;
 }
+return state;
+};
 
-render(<App />, window.document.getElementById('app'));
+const store = createStore( combineReducers({mathReducer,userReducer}));
+
+store.subscribe(() => {
+    console.log("Store updated", store.getState());
+});
+
+store.dispatch({
+    type:"ADD",
+    payload: 100
+});
+
+store.dispatch({
+    type:"ADD",
+    payload: 22
+});
+
+
+store.dispatch({
+    type:"SET_AGE",
+    payload: 30
+});
+
+// store.dispatch({
+//     type:"SUBTRACT",
+//     payload: 80
+// });
